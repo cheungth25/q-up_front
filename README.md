12a419e0437dad914da4703bf630a91c15ab2377
cheungth25 <cheungth25@gmail.com>
2017-10-30 12:08:14 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+Q-UP
---
+++ src/adapters/SignupAdapter.js
@@ -0,0 +1,14 @@
+export default class SignupAdapter {
+  static signup(data) {
+    return (
+      fetch("http://localhost:3000/api/v1/login", {
+        headers: {
+          'accept': 'application/json',
+          'content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(data)
+      }).then((data)=>{return data.json()}) //response from login request
+    )
+  }
+}
